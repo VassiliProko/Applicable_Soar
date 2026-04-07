7797232f79d44127bcd6e2bcc760663ece24af5f
VassiliProko <vassiligb12@gmail.com>
2026-04-07 17:48:58 +0800

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Playfair_Display } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import { MantineProvider } from "@mantine/core";
 import Navbar from "./components/Navbar";
 import LayoutFooter from "./components/LayoutFooter";
@@ -42,6 +43,7 @@ export default function RootLayout({
           {children}
           <LayoutFooter />
         </MantineProvider>
+        <Analytics />
       </body>
     </html>
   );
